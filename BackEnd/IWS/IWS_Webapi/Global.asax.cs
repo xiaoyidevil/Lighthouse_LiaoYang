f32ef27be2b1aa4eebf0147da878c6bbdb62926e
xiaoyidevil <ltt.007@163.com>
2022-06-25 14:26:33 +0800

    Update Global.asax.cs

--- BackEnd/IWS/IWS_Webapi/Global.asax.cs
+++ BackEnd/IWS/IWS_Webapi/Global.asax.cs
@@ -9,6 +9,13 @@ namespace IWS_Webapi
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST,PUT, DELETE");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        }
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
